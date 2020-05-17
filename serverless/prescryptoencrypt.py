f33c1468c63ee1b44e5db90d2d68cc14eeaa058a
Muntaser Syed <jemsbhai@gmail.com>
2020-05-17 09:52:03 -0400

    Create prescryptoencrypt.py

---
+++ serverless/prescryptoencrypt.py
@@ -0,0 +1,44 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+import json
+
+def enc (request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+
+    if request_json:
+        ciphertext = ""
+        keydata = request_json["key"]
+        keydata = keydata.encode()
+        plaintext = request_json["plaintext"]
+        plaintext = plaintext.encode()
+        
+        if request_json["key_type"] == 'public':
+            key = serialization.load_pem_public_key(
+            keydata, backend=default_backend())
+
+        if request_json["key_type"] == 'private':
+            key = serialization.load_pem_private_key(
+            keydata, password=None, backend=default_backend())
+        
+
+        ciphertext = key.encrypt(plaintext, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None ))
+        ciphertext = ciphertext.decode()
+        result = {}
+        result["ciphertext"] = str(ciphertext)
+        return json.dumps(result)
+
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
